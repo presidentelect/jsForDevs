6e69966f06773d22ad08219b9a34e6861c803cb2
Michael Hansen <m.hansen@pobox.com>
2024-11-07 13:52:08 -0500

    anonymous functions

---
+++ anonymous functions.js
@@ -0,0 +1,25 @@
+console.log('anonymous functions.js');
+console.log('----------------------');
+console.log('');
+
+// function definition
+function foo() {
+	console.log('foo');
+}
+
+// function expression
+var f = function express() {
+	console.log('exp');
+};
+
+// anonymous function
+var g = function () {
+	console.log('anonymous');
+};
+
+foo();
+f();
+express();
+g();
+
+
